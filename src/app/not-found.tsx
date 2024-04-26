15715871141201ddbf275cad1dd887e31db1ac4a
ronen <ronenbadalov1@gmail.com>
2024-04-27 01:21:19 +0300

    added redirects instead of 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { defaultPaths, paths } from "@/lib/paths";
+import { Role } from "@prisma/client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+export default function Custom404() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  if (!session) return router.push(paths.LOGIN);
+
+  if (session && session.user && !session.user.role) {
+    return router.push(paths.SELECT_ROLE);
+  }
+  if (session && session.user && session.user.role === Role.CUSTOMER) {
+    return router.push(defaultPaths.CUSTOMER);
+  }
+}
