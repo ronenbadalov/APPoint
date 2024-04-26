7da89e583f5abb93016445c440eead9ee393b94f
ronen <ronenbadalov1@gmail.com>
2024-04-26 17:50:05 +0300

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,3 @@
-// import { nextAuthOptions } from "@/lib/auth";
-// import { NextApiRequest, NextApiResponse } from "next";
-// import NextAuth from "next-auth/next";
-
-// function auth(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ): ReturnType<typeof NextAuth> {
-//   return NextAuth(req, res, nextAuthOptions(req, res));
-// }
-// export { auth as GET, auth as POST };
-
 import NextAuth from "next-auth";
 import { options } from "./options";
 
