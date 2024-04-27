9186b2887cc51ea90b0747b9a3e74dd370a99f9c
ronen <ronenbadalov1@gmail.com>
2024-04-27 17:21:25 +0300

    redirect to select role if has session with no role

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,13 @@
+import { options } from "@/app/api/auth/[...nextauth]/options";
+import { paths } from "@/lib/paths";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import LoginForm from "./form";
+
 export default async function LoginPage() {
+  const session = await getServerSession(options);
+  if (session && session.user && !session.user.role) {
+    redirect(paths.SELECT_ROLE);
+  }
   return <LoginForm />;
 }
